d50dce3bf5137c2a39cd37edaa7082009a16b568
fabri <fabritzioalejandrol@gmail.com>
2025-07-30 11:25:17 -0600

    Validar que los datos ingresados sean validos

--- actividad10.py
+++ actividad10.py
@@ -1,4 +1,23 @@
-productos={}
+inventario={}
 cantidad= int(input("Ingrese la cantidad de productos a ingresar: "))
 for i in range(cantidad):
     print(f"\nProducto {i+1}")
+    while True:
+        codigo = input("Ingrese codigo del producto: ")
+        nombre = input("Ingrese nombre del producto: ")
+        precio = float(input("Ingrese precio del producto: "))
+        stock = int(input("Ingrese cantidad de stock del producto: "))
+        talla = input("Ingrese la talla del producto: ").upper()
+        if codigo in inventario:
+            print("Codigo ya existente")
+            continue
+        if precio <=0:
+            print("Precio invalido")
+            continue
+        if stock <=0:
+            print("Cantidad invalida")
+            continue
+        if talla not in ["S","M","L","XL"]:
+            print("Talla invalida")
+            continue
+        break
